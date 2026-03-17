f759657b933413db98adedf4cc41cd9ab5cd0d17
k-p2606 <kaavyapatell26@gmail.com>
2026-03-17 17:05:06 -0600

    added buttons

--- src/Timer.js
+++ src/Timer.js
@@ -1,6 +1,27 @@
+import { CircularProgressbar, buildStyles} from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import PlayButton from './playButton';
+import PauseButton from './pauseButton';
+import SettingsButton from './settingsButton';
+
+const red = '#f54e4e';
+const green = '#4aec8c';
+
 function Timer() {
     return (
-        <div>timer</div>
+        <div>
+            <CircularProgressbar value={60} text={`60%`} styles={buildStyles({textColor: '#fff', pathColor:red, trailColor: 'rgba(255, 255,255, .2)'})}/>
+
+            <div style={{marginTop:'20px'}}>
+                <PlayButton/>
+                <PauseButton />
+            </div>
+            <div style={{marginTop: '20px'}}>
+                <SettingsButton />
+            </div>
+
+        </div>
+
     );
 }
 
